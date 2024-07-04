1afb3c1eb3744f92c7f3f4c6c6c817c3736b2057
Sanjai Satheesh <sanjaisatheesh002@gmail.com>
2024-07-04 21:28:41 +0530

    updated ml models

--- train/diabetics.py
+++ train/diabetics.py
@@ -4,28 +4,22 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import joblib
 
-# Load the dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
 names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age', 'Outcome']
 data = pd.read_csv(url, names=names)
 
-# Splitting the dataset into features and target variable
 X = data.drop('Outcome', axis=1)
 y = data['Outcome']
 
-# Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize the SVM Classifier
 svm_classifier = SVC(kernel='linear', probability=True, random_state=42)
 
-# Train the model
 svm_classifier.fit(X_train, y_train)
 
-# Make predictions
 y_pred = svm_classifier.predict(X_test)
 
-# Calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy of SVM Classifier:", accuracy)
 
